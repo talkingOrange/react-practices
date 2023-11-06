14da26708ea49bd502f037a71a4457423c561708
[talkingOrange] <sonrisa-bonita@naver.com>
2023-11-06 18:45:27 +0900

    [ feat ] json

---
+++ 2.component/ex06/03.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+
+let state = {
+    order: JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'))
+
+};
+
+const updateOrder = Object.assign({}, state.order, {
+    receive: '강남구 논현동'
+})
