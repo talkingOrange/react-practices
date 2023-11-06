39bfb2dbac296ea43a77638a3a15db5481f27679
[talkingOrange] <sonrisa-bonita@naver.com>
2023-11-06 18:45:44 +0900

    [ feat ] emaillist

---
+++ emaillist/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
@@ -0,0 +1,23 @@
+package com.poscodx.emaillist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poscodx.emaillist.dto.JsonResult;
+import com.poscodx.emaillist.repository.EmaillistRepository;
+
+@RestController
+public class ApiController {
+	@Autowired
+	private EmaillistRepository emaillistRepository;
+	
+	@GetMapping("/api")
+	public ResponseEntity<JsonResult> read() {
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(emaillistRepository.findAll()));
+	}
+}
--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,30 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./assets/scss/App.scss";
 import RegisterForm from "./RegisterForm";
 import SearchBar from "./SearchBar";
 import Emaillist from "./Emaillist";
-import data from "./assets/json/data";
+// import data from './assets/json/data';
 
 function App() {
-  const [emails, setEmails] = useState(data);
-  const searchEamil = (keyword) => {
-    const newEamils = data.filter(function (email) {
-      return (
+  const [emails, setEmails] = useState(null);
+  const searchEmail = (keyword) => {
+    const newEmails = data.filter(
+      (email) =>
         email.firstName.indexOf(keyword) !== -1 ||
         email.lastName.indexOf(keyword) !== -1 ||
         email.email.indexOf(keyword) !== -1
-      );
-    });
-
-    setEmails(newEamils);
-
-    console.log(emails);
+    );
+    setEmails(newEmails);
   };
+
+  const addEmail = async (email) => {
+    fetch("/api", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(email),
+    });
+  };
+
+  const fetchList = async () => {
+    try {
+      const response = await fetch("/api", {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: null,
+      });
+
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+
+      if (json.result !== "success") {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      console.log(json.data);
+      setEmails(json.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
     <div id={"App"}>
-      <RegisterForm />
-      <SearchBar searchEmail={searchEamil} />
-      <Emaillist emails={emails} />
+      <RegisterForm addEmail={addEmail} />
+      <SearchBar searchEmail={searchEmail} />
+      {emails === null ? null : <Emaillist emails={emails} />}
     </div>
   );
 }
